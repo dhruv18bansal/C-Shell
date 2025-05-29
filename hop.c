9578875e0b9e1bb93911e4efe22ddea0c438a56e
Dhruv Bansal <dhruvbansal@Dhruvs-MacBook-Air.local>
2025-05-29 17:34:53 +0530

    Implemented Hop Command

---
+++ hop.c
@@ -0,0 +1,13 @@
+#include "headers.h"
+#include "hop.h"
+#include "commands.h"
+
+void hop_command(char *args[], int arg_count) {
+    if (arg_count == 1) {
+        change_directory("~");
+    } else {
+        for (int j = 1; j < arg_count; j++) {
+            change_directory(args[j]);
+        }
+    }
+}
---
+++ hop.h
@@ -0,0 +1,9 @@
+#ifndef HOP_H
+#define HOP_H
+
+#include "headers.h"
+
+// Function to change directories according to the "hop" command
+void hop_command(char *args[], int arg_count);
+
+#endif // HOP_H
